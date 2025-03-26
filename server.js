fecb0b94ebc89250d87e3378991076edc9628969
Ayush Mittal <ayushmittalbkn@gmail.com>
2025-03-26 19:13:05 +0530

    debug1

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "*", // Allow all origins (or replace with your Vercel frontend URL)
+        origin: ["https://collab-draw-one.vercel.app"], // Add your Vercel URL here
         methods: ["GET", "POST"]
     }
 });
